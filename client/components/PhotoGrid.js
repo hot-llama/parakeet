b2873021c11595ce5af0e2105e2b0491bb3b3088
Josh Milo <josh@joshmilo.com>
2016-05-28 14:05:46 -0700

    switching project stuff

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div>
+        <div className="single-photo">
+          I'm the photo grid
+        </div>
+      </div>
+    )
+  }
+});
+
+export default PhotoGrid;
---
+++ client/reducers/comments.js
@@ -0,0 +1,6 @@
+function commentsReducer(state = [], action) {
+  console.log("COMM", state, action);
+  return state;
+}
+
+export default commentsReducer;
